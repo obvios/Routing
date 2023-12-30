8121ed06f628188d1dab2952625987d7cd87de73
Eric Palma <palmae95@gmail.com>
2023-12-30 11:56:25 -0500

    remove test

--- Tests/RoutingTests/RoutingTests.swift
+++ Tests/RoutingTests/RoutingTests.swift
@@ -2,10 +2,5 @@ import XCTest
 @testable import Routing
 
 final class RoutingTests: XCTestCase {
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct
-        // results.
-        XCTAssertEqual(Routing().text, "Hello, World!")
-    }
+    
 }
