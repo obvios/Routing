4b1e737cf88713e8d2c884fb2926a43fc6773fa7
Eric Palma <palmae95@gmail.com>
2024-01-01 14:11:58 -0500

    define Routable enum for testing

--- Tests/RoutingTests/RoutingTests.swift
+++ Tests/RoutingTests/RoutingTests.swift
@@ -1,6 +1,40 @@
 import XCTest
+import SwiftUI
 @testable import Routing
 
 final class RoutingTests: XCTestCase {
+    func testPush() {
+        
+    }
+}
+
+fileprivate enum TestRoute: Routable {
+    case viewA
+    case viewB
+    case viewC
+    
+    @ViewBuilder
+    func viewToDisplay(router: Router<Self>) -> some View {
+        switch self {
+        case .viewA:
+            Text("ViewA")
+        case .viewB:
+            Text("ViewB")
+        case .viewC:
+            Text("ViewC")
+        }
+    }
+    
+    var navigationType: Routing.NavigationType {
+        switch self {
+        case .viewA:
+            return .push
+        case .viewB:
+            return .sheet
+        case .viewC:
+            return .fullScreenCover
+        }
+    }
+    
     
 }
