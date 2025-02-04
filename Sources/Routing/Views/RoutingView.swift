c3b058c4a1512dd76870e1316e71527c0998befc
Eric Palma <ericpalma@Erics-Mac-mini.local>
2025-02-04 11:31:14 -0500

    initial support for deeplinking

--- Sources/Routing/Views/RoutingView.swift
+++ Sources/Routing/Views/RoutingView.swift
@@ -3,9 +3,13 @@ import SwiftUI
 public struct RoutingView<Content: View, Destination: Routable>: View {
     @StateObject var router: Router<Destination> = .init(isPresented: .constant(.none))
     private let rootContent: (Router<Destination>) -> Content
+    private let handleDeepLink: ((URL) -> Destination?)?
     
-    public init(_ routeType: Destination.Type, @ViewBuilder content: @escaping (Router<Destination>) -> Content) {
+    public init(_ routeType: Destination.Type,
+                @ViewBuilder content: @escaping (Router<Destination>) -> Content,
+                handleDeepLink: ((URL) -> Destination?)? = nil) {
         self.rootContent = content
+        self.handleDeepLink = handleDeepLink
     }
     
     public var body: some View {
@@ -20,6 +24,19 @@ public struct RoutingView<Content: View, Destination: Routable>: View {
         }
         .fullScreenCover(item: $router.presentingFullScreenCover) { route in
                 router.view(for: route)
+        }
+        .onOpenURL { url in
+            if let destination = handleDeepLink?(url) {
+                // Dismiss any modals before routing
+                if router.presentingSheet != nil {
+                    router.dismiss()
+                }
+                if router.presentingFullScreenCover != nil {
+                    router.dismiss()
+                }
+                
+                router.routeTo(destination)
+            }
         }
     }
 }
