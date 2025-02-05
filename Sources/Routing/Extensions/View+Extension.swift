f885cd22448920deb70732358bcf60fe0238308d
Eric Palma <ericpalma@Erics-Mac-mini.local>
2025-02-05 15:33:00 -0500

    add deep link modifier and view extension for modifier files

---
+++ Sources/Routing/Extensions/View+Extension.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  Routing
+//
+//  Created by Eric Palma on 2/5/25.
+//
+
+import Foundation
---
+++ Sources/Routing/Views/Modifiers/DeepLinkModifier.swift
@@ -0,0 +1,38 @@
+//
+//  File.swift
+//  Routing
+//
+//  Created by Eric Palma on 2/5/25.
+//
+
+import SwiftUI
+
+extension View {
+    func onDeepLink<Destination: Routable>(_ handleDeepLink: @escaping (URL) -> Destination?) -> some View {
+        self.modifier(DeepLinkModifier(handleDeepLink: handleDeepLink))
+    }
+}
+
+struct DeepLinkModifier<Destination: Routable>: ViewModifier {
+    @EnvironmentObject var router: Router<Destination>
+    let handleDeepLink: (URL) -> Destination?
+
+    func body(content: Content) -> some View {
+        content
+            .onOpenURL { url in
+                if let destination = handleDeepLink(url) {
+                    // Dismiss active modals before routing
+                    if router.presentingSheet != nil {
+                        router.dismiss()
+                    }
+                    if router.presentingFullScreenCover != nil {
+                        router.dismiss()
+                    }
+
+                    // Navigate to the deep link destination
+                    router.routeTo(destination)
+                }
+            }
+    }
+}
+
