421e8c6353ebbd57cb3d67decc45ef068069b64b
Eric Palma <ericpalma@Erics-Mac-mini.local>
2025-02-05 15:34:16 -0500

    remove old api for passing deep link handler

--- Sources/Routing/Views/RoutingView.swift
+++ Sources/Routing/Views/RoutingView.swift
@@ -3,13 +3,11 @@ import SwiftUI
 public struct RoutingView<Content: View, Destination: Routable>: View {
     @StateObject var router: Router<Destination> = .init(isPresented: .constant(.none))
     private let rootContent: (Router<Destination>) -> Content
-    private let handleDeepLink: ((URL) -> Destination?)?
     
     public init(_ routeType: Destination.Type,
                 @ViewBuilder content: @escaping (Router<Destination>) -> Content,
                 handleDeepLink: ((URL) -> Destination?)? = nil) {
         self.rootContent = content
-        self.handleDeepLink = handleDeepLink
     }
     
     public var body: some View {
@@ -24,19 +22,6 @@ public struct RoutingView<Content: View, Destination: Routable>: View {
         }
         .fullScreenCover(item: $router.presentingFullScreenCover) { route in
                 router.view(for: route)
-        }
-        .onOpenURL { url in
-            if let destination = handleDeepLink?(url) {
-                // Dismiss any modals before routing
-                if router.presentingSheet != nil {
-                    router.dismiss()
-                }
-                if router.presentingFullScreenCover != nil {
-                    router.dismiss()
-                }
-                
-                router.routeTo(destination)
-            }
         }
     }
 }
